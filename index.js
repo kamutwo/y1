301cc461b645f764d282b3deb667fa737961b472
youtwo_ <ilhamcodes@gmail.com>
2023-10-07 21:09:12 +0700

    add server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import { ShardingManager } from 'discord.js';
+import { serve } from '@hono/node-server';
 import { config } from 'dotenv';
+import app from './src/server.js';
+
 config();
+serve(app, (info) => {
+	console.log(`[Hono]: Listening on http://localhost:${info.port}`);
+});
 
 const manager = new ShardingManager('./src/Bot.js', { token: process.env.Token, totalShards: 'auto' });
 manager.on('shardCreate', (shard) => {
